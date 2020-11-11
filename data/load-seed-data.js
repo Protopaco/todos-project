6af3d64a3afe32967eaf3040bf01417cc76abe13
Paul Stevens <twinkisan@hotmail.com>
2020-11-10 16:58:27 -0800

    now finished with /data

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const usersData = require('./users.js');
+// const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
 run();
@@ -10,16 +10,16 @@ async function run() {
   try {
     await client.connect();
 
-    const users = await Promise.all(
-      usersData.map(user => {
-        return client.query(`
-                      INSERT INTO users (email, hash)
-                      VALUES ($1, $2)
-                      RETURNING *;
-                  `,
-          [user.email, user.hash]);
-      })
-    );
+    // const users = await Promise.all(
+    //   usersData.map(user => {
+    //     return client.query(`
+    //                   INSERT INTO users (email, hash)
+    //                   VALUES ($1, $2)
+    //                   RETURNING *;
+    //               `,
+    //       [user.email, user.hash]);
+    //   })
+    // );
 
     // const user = users[0].rows[0];
 
